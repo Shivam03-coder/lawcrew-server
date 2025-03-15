acb6711b0e43351c73fee36068e17067c4f23bbc
shivam anand <shivam850anand@gmail.com>
2025-03-15 15:39:32 +0530

    ai added to codebase

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -9,4 +9,5 @@ export const appEnvConfigs = {
   SIGNING_SECRET: process.env.SIGNING_SECRET,
   ENCRYPTION_kEY: process.env.ENCRYPTION_kEY,
   ARC_JET_PUBLISHABLE_KEY: process.env.ARC_JET_PUBLISHABLE_KEY,
+  AI_PUBLISHABLE_KEY: process.env.AI_PUBLISHABLE_KEY,
 };
